fdb908ed3a219fbe28a0a5890cd71c79c555aa23
Seunggyun <s.han@aresa.io>
2023-07-25 16:59:47 +0900

    :tada: create a project template

---
+++ src/sketch.js
@@ -0,0 +1,12 @@
+const width = 600;
+const height = 600;
+const centerX = width / 2;
+const centerY = height / 2;
+
+function setup() {
+  createCanvas(width, height);
+}
+
+function draw() {
+  background(255);
+}
